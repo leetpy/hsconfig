a4d37db902c3bd71ad9660709ec63dffff65eb4e
yuanliang <yuanliang01@360.net>
2019-08-06 11:02:20 +0800

    update

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ SCRIPT_HOME=$(cd $(dirname $0) && pwd)
 function main {
     if [ -f "$HOME/.hammerspoon/init.lua" ]; then
         echo "backup old hs config"
-        mv "$HOME/.hammerspoon/init.lua" "$HOME/.hammerspoon/init.lua.bak"
+        mv -f "$HOME/.hammerspoon/init.lua" "$HOME/.hammerspoon/init.lua.bak"
         cp $SCRIPT_HOME/init.lua "$HOME/.hammerspoon/init.lua"
     fi
 }
